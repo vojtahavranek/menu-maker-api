e3c3178a6dd692d239fa207b6aab14dc229084e9
Vojtech Havranek <havranek@ebrana.cz>
2020-02-07 22:45:55 +0100

    Handling localhost cors (adding nelmio cosr bundle)

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
     TheCodingMachine\Graphqlite\Bundle\GraphqliteBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
